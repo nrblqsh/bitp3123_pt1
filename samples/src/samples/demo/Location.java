1385a8e61cdd4593fd2bb0b1baf5d929eb52c67b
nrblqsh <b032110395@student.utem.edu.my>
2023-05-07 22:02:51 +0800

    Location Commit

---
+++ samples/src/samples/demo/Location.java
@@ -0,0 +1,19 @@
+package samples.demo;
+
+public class Location {
+	
+	private int test;
+	
+	public int loc;
+
+	public int getLoc() {
+		return loc;
+	}
+
+	public void setLoc(int loc) {
+		this.loc = loc;
+	}
+	
+	
+
+}
