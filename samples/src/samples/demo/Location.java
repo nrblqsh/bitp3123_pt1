12158c7d42d9cef2b7ecd03947cea6be5eab3067
nrblqsh <b032110395@student.utem.edu.my>
2023-05-07 22:05:21 +0800

    Location Commit after remove attribute name

--- samples/src/samples/demo/Location.java
+++ samples/src/samples/demo/Location.java
@@ -4,6 +4,27 @@ public class Location {
 	
 	private int test;
 	
+	private int Locationid;
+	private int description;
+	
+
+
+	public int getLocationid() {
+		return Locationid;
+	}
+
+	public void setLocationid(int locationid) {
+		Locationid = locationid;
+	}
+
+	public int getDescription() {
+		return description;
+	}
+
+	public void setDescription(int description) {
+		this.description = description;
+	}
+
 	public int loc;
 
 	public int getLoc() {
